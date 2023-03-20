207c2f7bf9186eff7bbe8216b318167fd104beb1
eunbi <eunbi264@naver.com>
2023-03-20 14:33:54 +0900

    create catkin workspace

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/eunbi/self_driving_PJT/catkin_ws/src
+# Build directory: /home/eunbi/self_driving_PJT/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("step_1")
